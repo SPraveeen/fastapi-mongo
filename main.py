1abcb6fefd3c234dccf8ff7143e0070d13bc05de
SPraveeen <praveenji8498@gmail.com>
2025-05-28 14:58:46 +0530

    error after giving correctusername and password

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
-# from pymongo import server_api
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-
+from pymongo.errors import ConfigurationError, OperationFailure
 
 app = FastAPI()
 
+uri = "mongodb+srv://praveenbyju:Praveen1999@cluster0.ltcoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-uri = "mongodb+srv://praveenbyju:praveen1999@@cluster0.ltcoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
